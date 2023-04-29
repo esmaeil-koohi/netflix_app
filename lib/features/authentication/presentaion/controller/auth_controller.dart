0fe4b1bdab9ab2891c569940420260cdb2b0cd0a
esmaeil-koohi <esmaeil76koohi@gmail.com>
2023-04-29 14:17:38 +0430

    login ui refactor

--- lib/features/authentication/presentaion/controller/auth_controller.dart
+++ lib/features/authentication/presentaion/controller/auth_controller.dart
@@ -1,21 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:netflix_proj/core/data/catch/cache_provider.dart';
 import 'package:netflix_proj/core/exceptions/failure_exception.dart';
 import 'package:netflix_proj/features/authentication/domain/entities/login_entity.dart';
 import 'package:netflix_proj/features/authentication/domain/usecase/login_usecase.dart';
 
-class AuthController extends GetxController{
+class AuthController extends GetxController {
   final LoginUseCase loginUseCase;
+
   AuthController({required this.loginUseCase});
 
   RxString email = ''.obs;
   RxString password = ''.obs;
   RxBool isLoading = true.obs;
+  RxBool isPasswordShow = true.obs;
+  GlobalKey<FormState> formKey = GlobalKey();
 
-  void setLogin() async{
+  void setLogin() async {
     isLoading.value = false;
-    Either<Failure, LoginEntity> response = await loginUseCase({"email" : email.value, "password" : password.value});
+    Either<Failure, LoginEntity> response =
+        await loginUseCase({"email": email.value, "password": password.value});
     response.fold((error) {
       isLoading.value = true;
       print(error.response);
@@ -26,4 +31,23 @@ class AuthController extends GetxController{
     });
   }
 
+  bool changePasswordIcon() {
+    return isPasswordShow.value = !isPasswordShow.value;
+  }
+
+  String? validateEmailField(final String value) {
+    if (value.trim().isEmpty) {
+      return 'The Email field cannot be empty';
+    }
+    return null;
+  }
+
+  String? validatePasswordField(final String value) {
+    if (value.trim().isEmpty) {
+      return 'The password field cannot be empty';
+    } else if (value.trim().length < 3) {
+      return 'The length of the password must be at least 4 characters';
+    }
+    return null;
+  }
 }
--- lib/features/authentication/presentaion/pages/login_screen.dart
+++ lib/features/authentication/presentaion/pages/login_screen.dart
@@ -1,46 +1,100 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:netflix_proj/features/authentication/presentaion/controller/auth_controller.dart';
 
 class LoginScreen extends GetView<AuthController> {
-   LoginScreen({Key? key}) : super(key: key);
-
-  final TextEditingController _controller = TextEditingController();
-
+    const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
-        child:SingleChildScrollView(
-          child:Obx(()=>  Center(
-            child:  Form(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextFormField(
-                      initialValue: controller.email.value,
-                      onChanged: (value) => controller.email.value = value,
-                      decoration: const InputDecoration(
-                        hintText: 'Email',
-                        border: OutlineInputBorder()
-                      ),
+        child:Stack(
+          children: [
+            Positioned.fill(
+                child: Container(
+                  decoration: const BoxDecoration(
+                   image: DecorationImage(
+                     image: ExactAssetImage('assets/login2.png'),
+                     fit: BoxFit.fill
+                   )
+                  ),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 30,
+                      sigmaY: 30,
                     ),
-                    const SizedBox(height: 30,),
-                    TextFormField(
-                      initialValue: controller.password.value,
-                      onChanged: (value) => controller.password.value = value,
+                    child: Container(
+                      color: Colors.black12,
                     ),
-                    const SizedBox(height: 30,),
-                    controller.isLoading.isFalse ? const Center(child: CircularProgressIndicator(),) : ElevatedButton(onPressed: () {
-                       controller.setLogin;
-                    }, child: const Text('وارد شدن')),
-                  ],)),
-          ),)
+                  ),
+                )
+            ),
+            Positioned(
+             left: 40,
+             right: 40,
+             top: height * 0.15,
+              child:Container(
+              width: width * 0.6,
+              height: height * 0.6,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 30, 10, 10),
+                  child: Obx(() => Form(
+                    key: controller.formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          initialValue: controller.email.value,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (value) => controller.validateEmailField(value!),
+                          onChanged: (value) => controller.email.value = value,
+                          decoration: const InputDecoration(
+                              hintText: 'Email',
+                              border: OutlineInputBorder()
+                          ),
+                        ),
+                        const SizedBox(height: 20,),
+                        TextFormField(
+                          maxLength: 10,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (value) => controller.validatePasswordField(value!),
+                          initialValue: controller.password.value,
+                          onChanged: (value) => controller.password.value = value,
+                          decoration:  InputDecoration(
+                              suffixIcon: GestureDetector(
+                                child: controller.isPasswordShow.value ?
+                                const Icon(Icons.visibility) :
+                                const Icon(Icons.visibility_off),
+                                onTap: ()=> controller.changePasswordIcon(),
+                              ),
+                              hintText: 'Password',
+                              border: const OutlineInputBorder()
+                          ),
+                          obscureText: controller.isPasswordShow.value,
+                        ),
+                        controller.isLoading.isFalse ? const Center(child: CircularProgressIndicator(),) :
+                        ElevatedButton(onPressed: () {
 
-        ),
+                        }, child: const Text('Login', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)),
+                      ],
+                    ),
+                  ),),
+                )
+            ),)
+          ],
+        )
       ),
     );
   }
 }
+
